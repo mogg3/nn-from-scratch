a2726cef2a50aa9b439ab3c6615b7fa28ef7fab2
MarcusE-creator <70136517+MarcusE-creator@users.noreply.github.com>
2022-02-11 15:12:39 +0100

    small changes

--- classes/Layer_Dense.py
+++ classes/Layer_Dense.py
@@ -1,15 +1,19 @@
 import numpy as np
 
 class Layer_Dense:
-    def __init__(self, n_inputs, n_outputs, activation, random_state=0):
+    def __init__(self, n_inputs, n_outputs, activation, random_state = 0):
         np.random.seed(random_state)
         self.n_inputs = n_inputs
         self.n_outputs = n_outputs
+        
+        # Parameter initialization. Values are taken from a uniform distribution between 0,1. 
+        # Subtract 0.5 to get values between -0.5, 0.5
+        
         self.W = np.random.rand(n_outputs, n_inputs) - 0.5
         self.b = np.random.rand(n_outputs, 1) - 0.5
-
+        
         self.activation = activation
-
+        
         self.inputs = None
         self.Z = None
         self.A = None
--- classes/Net.py
+++ classes/Net.py
@@ -6,8 +6,9 @@ class Net:
     def __init__(self, random_state):
         self.layers = []
         self.acc_history = None
+        self.loss_history = None
         self.random_state = random_state
-
+        
     def add_layer(self, n_inputs, n_outputs, activation):
         if activation == 'ReLU':
             activation = self.ReLU
@@ -17,7 +18,7 @@ class Net:
             activation = self.softmax
         else:
             raise Exception(f"{activation} is not a valid activation function")
-
+                            
         if len(self.layers) > 0:
             if n_inputs == self.layers[-1].n_outputs:
                 layer = Layer_Dense(n_inputs, n_outputs, activation, self.random_state)
@@ -25,42 +26,46 @@ class Net:
                 raise Exception('Invalid layer dims')
         else:
             layer = Layer_Dense(n_inputs, n_outputs, activation)
-
+            
         self.layers.append(layer)
-
-    def error(self, outputs, one_hot_y):
-        return outputs - one_hot_y
-
-    def ReLU(self, Z, derive=False):
+                  
+            
+    def ReLU(self, Z, derive = False):
         if not derive:
             return np.maximum(Z, 0)
         else:
             return Z > 0
-
-    def sigmoid(self, Z, derive=False):
+        
+    def sigmoid(self, Z, derive = False):
         if not derive:
-            return 1 / (1 + np.exp(-Z))
+            return 1/(1 + np.exp(-Z))
         else:
-            y = 1 / (1 + np.exp(-Z))
+            y = 1/(1 + np.exp(-Z))
             yp = y * (1.0 - y)
             return yp
-
+    
     def softmax(self, Z):
         A = np.exp(Z) / sum(np.exp(Z))
         return A
-
+    
     def one_hot(self, Y, n_classes):
         one_hot_Y = np.zeros((Y.size, n_classes))
         one_hot_Y[np.arange(Y.size), Y] = 1
         one_hot_Y = one_hot_Y.T
         return one_hot_Y
-
+    
+    def CCE(self, pred, label, m):
+        loss = label * -np.log(pred)
+        # Average over all samples in the batch
+        loss = np.sum(np.sum(loss, axis = 1))/m
+        return loss
+    
     def update_params(self, layer, dW, db, learning_rate):
         layer.W -= learning_rate * dW
         layer.b -= learning_rate * db
-
+        
     def forward(self, inputs):
-        for i, layer in enumerate(self.layers):
+        for i, layer in enumerate(self.layers):    
             layer.inputs = inputs
             Z = layer.W.dot(inputs) + layer.b
             A = layer.activation(Z)
@@ -68,38 +73,42 @@ class Net:
             layer.A = A
             inputs = A
         return A
-
+    
     def backward(self, outputs, Y, learning_rate, n_classes):
-        one_hot_y = self.one_hot(Y, n_classes)
+        label_one_hot = self.one_hot(Y, n_classes)  
         m = outputs.shape[1]
-
+        
         for i in reversed(range(len(self.layers))):
             layer = self.layers[i]
-            if i == len(self.layers) - 1:
-                dZ = self.error(outputs, one_hot_y)
+            if i == len(self.layers)-1:
+                # dl/dz
+                dZ = outputs - label_one_hot
+                loss = self.CCE(outputs, label_one_hot, m)
             else:
-                dZ = self.layers[i + 1].W.T.dot(dZ) * layer.activation(layer.Z, derive=True)
-
-            dW = 1 / m * dZ.dot(layer.inputs.T)
-            db = 1 / m * np.sum(dZ, axis=1, keepdims=True)
-
+                dZ = self.layers[i+1].W.T.dot(dZ) * layer.activation(layer.Z, derive = True)
+            
+            dW = 1/m * dZ.dot(layer.inputs.T)
+            db = 1/m * np.sum(dZ, axis = 1, keepdims = True)
+            
             self.update_params(layer, dW, db, learning_rate)
-
+        return loss
+    
     def get_predictions(self, prediction):
         return np.argmax(prediction, 0)
-
+    
     def score(self, X, Y):
         predictions = self.get_predictions(self.predict(X))
         return np.sum(predictions == Y) / Y.size
-
-    def fit(self, X, Y, X_valid, y_valid, n_classes, epochs, batchsize=None, learning_rate=1):
-
+                
+    def fit(self, X, Y, X_valid, y_valid, n_classes, epochs, batchsize = None,  learning_rate = 1):
+        
         if not batchsize:
             batchsize = X.shape[1]
-
-        n_iterations = abs(math.floor(-X.shape[1] / batchsize)) * epochs
-        accuracy = np.zeros(n_iterations)
-
+            
+        n_iterations = abs(math.floor(-X.shape[1]/batchsize)) * epochs
+        accuracy = np.zeros(n_iterations)    
+        loss = np.zeros(n_iterations)
+        
         iteration = 0
         for epoch in range(epochs):
             for start_idx in range(0, X.T.shape[0], batchsize):
@@ -108,19 +117,24 @@ class Net:
 
                 inputs = X.T[excerpt].T
                 targets = Y.T[excerpt].T
-
+            
                 predictions = self.forward(inputs)
-                self.backward(predictions, targets, learning_rate, n_classes)
+                
+                l = self.backward(predictions, targets, learning_rate, n_classes)
                 a = self.score(X_valid, y_valid)
+                
                 accuracy[iteration] = a
-
-                if iteration % 25 in [0, 25]:
-                    print(f'Epoch {epoch + 1} Iteration {iteration}/{n_iterations}')
-                    print(f'Accuracy: {round(a, 3)}\n-')
-
+                loss[iteration] = l
+                
+                if iteration % 25 in [0,25]:
+                    print(f'Epoch {epoch+1} Iteration {iteration}/{n_iterations}')
+                    print(f'Accuracy: {round(a, 3)}')
+                    print(f'Loss: {l}\n-')
+                    
                 iteration += 1
-
+                
         self.acc_history = accuracy
-
-    def predict(self, X):
+        self.loss_history = loss
+            
+    def predict(self, X): 
         return self.forward(X)
